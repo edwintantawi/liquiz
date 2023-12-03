f9950d89d196b784d4470de949f1c0783df61757
edwintantawi <edwintantawi.dev@gmail.com>
2023-12-04 00:43:08 +0700

    feat: add app bar brand logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+import Link from 'next/link';
+
 import { GeistMono } from 'geist/font/mono';
 import { GeistSans } from 'geist/font/sans';
 
+import { Icons } from '~/components/icons';
+import { Button } from '~/components/ui/button';
 import { cn } from '~/lib/utils';
 
 import '~/app/style.css';
@@ -11,8 +15,21 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className={cn(GeistSans.variable, GeistMono.variable)}>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(GeistSans.variable, GeistMono.variable, 'h-full')}
+    >
+      <body className="mx-auto flex h-full max-w-md flex-col">
+        <div className="border-b p-3">
+          <Button asChild size="icon">
+            <Link href="/">
+              <Icons.Brand className="text-white" />
+              <span className="sr-only">LiQuiz</span>
+            </Link>
+          </Button>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
