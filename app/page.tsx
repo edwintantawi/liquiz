25992a621ad1faadac8191491f00b327054a4298
edwintantawi <edwintantawi.dev@gmail.com>
2023-12-04 18:48:29 +0700

    feat: add user profile ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,32 @@
-export default function RootPage() {
-  return <h1>Hello, Liquiz!</h1>;
+import * as React from 'react';
+
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+
+export const metadata = {
+  title: 'Dashboard',
+  description: 'Manage all your subjects and topics easily',
+};
+
+export default async function RootPage() {
+  return (
+    <>
+      <h1 className="sr-only">LiQuiz Dashboard</h1>
+      <div className="space-y-6">
+        <div className="space-y-2">
+          <div className="grid grid-cols-[auto,1fr] items-center gap-3 rounded-md border px-4 py-3">
+            <Avatar>
+              <AvatarImage src="" alt="" />
+              <AvatarFallback>LQ</AvatarFallback>
+            </Avatar>
+            <div>
+              <h2 className="font-semibold">LiQuiz User</h2>
+              <span className="rounded-full border bg-muted px-3 py-1 text-xs text-muted-foreground">
+                user@liquiz.com
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
