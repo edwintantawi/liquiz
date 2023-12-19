25aec8be230f715bdd6faf03709233bfb2ead70b
edwintantawi <edwintantawi.dev@gmail.com>
2023-12-19 18:46:05 +0700

    feat: allow header to accept children

--- components/header.tsx
+++ components/header.tsx
@@ -3,13 +3,18 @@ import * as React from 'react';
 interface HeaderProps {
   title: string;
   description: string;
+  children?: React.ReactNode;
 }
 
-export function Header({ title, description }: HeaderProps) {
+export function Header({ title, description, children }: HeaderProps) {
   return (
-    <header className="mb-4">
-      <h1 className="text-2xl font-bold">{title}</h1>
-      <p className="text-sm text-muted-foreground">{description}</p>
+    <header className="mb-4 grid grid-cols-[1fr,auto] items-center">
+      <div>
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-sm text-muted-foreground">{description}</p>
+      </div>
+
+      <div>{children}</div>
     </header>
   );
 }
