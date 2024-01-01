c2c4382b2355d96c0f5b02bac9349d5d5a7c91ff
edwintantawi <edwintantawi.dev@gmail.com>
2024-01-01 16:04:43 +0700

    feat: protect topics routes

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/subjects/:path*'],
+  matcher: ['/dashboard/:path*', '/subjects/:path*', '/topics/:path*'],
 };
