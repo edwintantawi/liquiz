53731d5ceb761ff0268804bad63c9c51faf434ff
edwintantawi <edwintantawi.dev@gmail.com>
2024-01-03 11:26:40 +0700

    feat(infra): initialize terraform with gcp provider

---
+++ infrastructure/variables.tf
@@ -0,0 +1,14 @@
+variable "project_region" {
+  type        = string
+  description = "Google Cloud Project Region"
+}
+
+variable "project_zone" {
+  type        = string
+  description = "Google Cloud Project Zone"
+}
+
+variable "project_id" {
+  type        = string
+  description = "Google Cloud Project ID"
+}
