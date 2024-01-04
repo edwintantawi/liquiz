0af44a0fa8f57b3d43b854b6293ffc4454ad001e
edwintantawi <edwintantawi.dev@gmail.com>
2024-01-04 14:53:52 +0700

    feat: add pubsub emulator script

---
+++ scripts/pubsub-emulator-subscription.sh
@@ -0,0 +1,3 @@
+curl -s -X PUT 'http://localhost:8043/v1/projects/liquiz/subscriptions/mysub' \
+    -H 'Content-Type: application/json' \
+    --data '{"topic":"projects/liquiz/topics/tasks-local","pushConfig":{"pushEndpoint":"http://localhost:3000/api/tasks"}}'
---
+++ scripts/pubsub-emulator.sh
@@ -0,0 +1,3 @@
+gcloud beta emulators pubsub start \
+  --project=liquiz \
+  --host-port=localhost:8043
